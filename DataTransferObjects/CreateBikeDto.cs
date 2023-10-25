a66fed7ca7cf970837adc819b3d5124536a10a17
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2023-10-25 15:45:10 -0700

    add data annotations on create bike dto

--- DataTransferObjects/CreateBikeDto.cs
+++ DataTransferObjects/CreateBikeDto.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RESTfulAPI.DataTransferObjects;
 
 public record CreateBikeDto
 {
+    [Required]
     public string? Brand { get; init; }
+
+    [Required]
     public string? Model { get; init; }
+
+    [Required]
+    [Range(1900, 2100)]
     public int Year { get; init; }
+
+    [Required]
     public string? Material { get; init; }
+
+    [Required]
     public string? Color { get; init; }
+
+    [Required]
     public string? Size { get; init; }
+
     public string? SerialNumber { get; init; }
 }
