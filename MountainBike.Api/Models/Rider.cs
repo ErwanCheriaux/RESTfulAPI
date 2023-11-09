2cabd438d37e8131de45ca4d1fd9ce260b7e6f3a
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2023-11-08 16:25:45 -0800

    rider store year of birth rather than current age

--- MountainBike.Api/Models/Rider.cs
+++ MountainBike.Api/Models/Rider.cs
@@ -4,7 +4,12 @@ public class Rider
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
-    public int Age { get; set; }
+    private int _yearOfBirth;
+    public int Age
+    {
+        get { return DateTime.Now.Year + _yearOfBirth; }
+        set { _yearOfBirth = DateTime.Now.Year - Age; }
+    }
     public string? Country { get; set; }
     public DateTimeOffset CreationDate { get; set; }
 }
