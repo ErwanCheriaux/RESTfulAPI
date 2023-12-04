f843e41117b67e24e285b54679bc78deee82ade8
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2023-12-03 18:15:34 -0800

    add tabs and tab

--- src/web-app/src/App.jsx
+++ src/web-app/src/App.jsx
@@ -1,9 +1,24 @@
+import { Tab, Tabs } from 'react-bootstrap';
 import BikeTable from './Components/BikeTable';
 
 export default function App() {
   return (
     <div className="App">
-      <BikeTable />
+      <Tabs
+        defaultActiveKey="home"
+        // id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          Tab content for Home
+        </Tab>
+        <Tab eventKey="bike" title="Bike">
+          <BikeTable />
+        </Tab>
+        <Tab eventKey="rider" title="Rider">
+          Tab content for Rider
+        </Tab>
+      </Tabs>
     </div>
   );
 }
