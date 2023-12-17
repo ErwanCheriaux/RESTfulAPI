203fd93a5cf7ba15cb0da6f3fa030acc3a3642f5
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2023-12-16 18:17:46 -0800

    refactor bike select

--- frontend/src/components/BikeSelect.jsx
+++ frontend/src/components/BikeSelect.jsx
@@ -1,28 +1,16 @@
-import { useState, useEffect } from 'react'
 import { Form } from 'react-bootstrap'
-import { getRiderBikesAsync, patchRiderBikeAsync } from '../api'
+import { patchRiderBikeAsync } from '../api'
 
 export default function BikeSelect({ riderId, bikes, loadBikes }) {
-    const [riderBikes, setRiderBikes] = useState([])
-
-    useEffect(() => {
-        loadRiderBikes()
-    }, [])
-
-    async function loadRiderBikes() {
-        const data = await getRiderBikesAsync(riderId)
-        setRiderBikes(data)
-    }
-
     async function handleChange(event) {
         const { value } = event.target
         await patchRiderBikeAsync(riderId, value)
-        await loadRiderBikes()
         await loadBikes()
     }
 
     function getSelectedBikeId() {
-        return riderBikes.length > 0 ? riderBikes[0].id : ''
+        const riderBike = bikes.find((bike) => bike.riderId === riderId)
+        return riderBike ? riderBike.id : ''
     }
 
     function isBikeAvailable(bike) {
