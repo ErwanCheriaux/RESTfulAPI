2ab28077a49d66a4c93106018f8e336ab226903b
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2023-12-27 00:08:38 -0800

    display login or logout only
    display login if not logged in yet
    display logout if logged in already

--- frontend/src/components/MainNavigation.jsx
+++ frontend/src/components/MainNavigation.jsx
@@ -8,10 +8,20 @@ import {
     Navbar,
 } from "react-bootstrap"
 
+import { getAuthToken } from "../utils/auth"
+
 export default function MainNavigation() {
     const submit = useSubmit()
     function handleLogout() {
         submit(null, { action: '/logout', method: 'post' })
+    }
+
+    function isLoggedIn() {
+        const token = getAuthToken()
+        if (token === null) {
+            return false
+        }
+        return true
     }
 
     return (
@@ -22,8 +32,10 @@ export default function MainNavigation() {
                 <NavLink to="/riders" className="nav-link" >Riders</NavLink>
             </Nav>
             <Nav>
-                <NavLink to="/auth" className="nav-link" >Login</NavLink>
-                <NavLink onClick={handleLogout} className="nav-link" >Logout</NavLink>
+                {isLoggedIn() ?
+                    <NavLink onClick={handleLogout} className="nav-link" >Logout</NavLink> :
+                    <NavLink to="/auth" className="nav-link" >Login</NavLink>
+                }
             </Nav>
         </Navbar>
     )
